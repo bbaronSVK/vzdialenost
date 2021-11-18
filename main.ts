d91c8396b95c17e1f6b803ad4d678dd59bf648ca
Michal Paulovic <coolmip@gmail.com>
2021-11-18 16:40:28 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
+function zastav () {
+    chod = 0
+    RingbitCar.brake()
+}
 input.onButtonPressed(Button.A, function () {
     chod = 1
     basic.pause(500)
     RingbitCar.forward()
 })
 input.onSound(DetectedSound.Loud, function () {
-    chod = 0
+    zastav()
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    chod = 0
+    zastav()
 })
 let chod = 0
 RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
@@ -16,12 +20,22 @@ chod = 0
 basic.forever(function () {
     if (chod == 1) {
         if (RingbitCar.ringbitcar_sonarbit(RingbitCar.Distance_Unit.Distance_Unit_cm) <= 10) {
-            RingbitCar.brake()
+            RingbitCar.back()
+            basic.pause(500)
+            RingbitCar.turnleft()
+            basic.pause(100)
         }
         if (RingbitCar.ringbitcar_sonarbit(RingbitCar.Distance_Unit.Distance_Unit_cm) > 10) {
             RingbitCar.forward()
         }
-    } else {
-        RingbitCar.brake()
+        if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_2)) {
+            RingbitCar.freestyle(100, 0)
+            basic.pause(100)
+        }
+        if (RingbitCar.tracking(RingbitCar.TrackingStateType.Tracking_State_1)) {
+            RingbitCar.freestyle(0, 100)
+            basic.pause(100)
+        }
+        RingbitCar.freestyle(80, 80)
     }
 })
